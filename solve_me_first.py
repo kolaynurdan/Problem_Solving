4cad4625d58e7dde05eab970bee5f98554e5b9e9
nurdan <kolaynurdan@gmail.com>
2022-08-25 16:45:54 +0300

    Create solve_me_first.py

---
+++ solve_me_first.py
@@ -0,0 +1,8 @@
+#Complete the function solveMeFirst to compute the sum of two integers.
+def solveMeFirst():
+    a=int(input())
+    b=int(input())
+    sum = (a+b)
+    return(print(sum))
+    
+solveMeFirst()
